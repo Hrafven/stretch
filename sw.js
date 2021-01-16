2971fa191b7dc61106ea1ab7bee739b10c314737
Hrafven <hrafven@gmail.com>
2021-01-16 16:18:10 +0100

    Update service worker error handling

--- sw.js
+++ sw.js
@@ -27,7 +27,16 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-    );
+    var response;
+    event.respondWith(caches.match(event.request).catch(function() {
+        return fetch(event.request);
+    }).then(function(r) {
+        response = r;
+        caches.open('v1').then(function(cache) {
+            cache.put(event.request, response);
+        });
+        return response.clone();
+    }).catch(function() {
+        return caches.match('/stretch/index.html');
+    }));
 });
